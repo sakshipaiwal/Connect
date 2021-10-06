d54b1c7618bd552880121ed9d36421557f516879
rjayeshk <rjlinkin50@gmail.com>
2021-10-07 01:19:14 +0530

    task component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { JournalComponent } from './journal/journal.component';
+import { TrackerComponent } from './tracker/tracker.component';
+import { TaskComponent } from './tracker/task/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    JournalComponent
+    JournalComponent,
+    TrackerComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
